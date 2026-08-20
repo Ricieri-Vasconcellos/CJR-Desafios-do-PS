8a63e7c72e366becca9adc0980d3c2c4d44b085b
Ricieri Vasconcellos <ricieribressiani07@gmail.com>
2026-08-20 10:43:04 -0300

    docs: Comenta a função

--- Pergunte_a_foca/script.js
+++ Pergunte_a_foca/script.js
@@ -1,9 +1,11 @@
 function escolhaAleatoria() {
 
+    // Escolha
+    
     var escolha = Math.floor(Math.random() * 10);
-
+    
     if (escolha % 2)
-        alert("A foca escolheu " + document.querySelector(".input1").value)
+        alert("A foca escolheu " + document.querySelector(".input1").value);
     else
-        alert("A foca escolheu " + document.querySelector(".input2").value)
+        alert("A foca escolheu " + document.querySelector(".input2").value);
 }
