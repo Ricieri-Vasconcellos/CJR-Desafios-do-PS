69a3a815e77ead3e3aace68b5deca3e2b1b398bc
Ricieri Vasconcellos <ricieribressiani07@gmail.com>
2026-08-21 21:41:47 -0300

    style: identação do codigo

--- Pergunte_a_foca/script.js
+++ Pergunte_a_foca/script.js
@@ -1,11 +1,9 @@
 function escolhaAleatoria() {
+  // Escolha
 
-    // Escolha
+  var escolha = Math.floor(Math.random() * 10);
 
-    var escolha = Math.floor(Math.random() * 10);
-
-    if (escolha % 2)
-        alert("A foca escolheu " + document.querySelector(".input1").value);
-    else
-        alert("A foca escolheu " + document.querySelector(".input2").value);
+  if (escolha % 2)
+    alert("A foca escolheu " + document.querySelector(".input1").value);
+  else alert("A foca escolheu " + document.querySelector(".input2").value);
 }
